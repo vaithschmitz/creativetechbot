19c62ac1869d32490266f2b22cfc91e293e98ac8
Vaith Schmitz <vaithschmitz@googlemail.com>
2019-06-21 10:54:45 +0100

    fixed querying

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ var retweet = function() {
     function randQ(){
         let q = '#creativetechnology, #creativetech, #techinnovation, #digitalinnovation, #innovationlab'
         let arr = q.split(',')
-        let randomQ = [Math.floor(Math.random()*arr.length)]
+        let randomQ = arr[Math.floor(Math.random()*arr.length)]
         return {
             q: randomQ,  
             result_type: 'recent',
@@ -24,19 +24,19 @@ var retweet = function() {
         if (!err) {
           // grab ID of tweet to retweet
           console.log(data)
-            // var retweetId = data.statuses[0].id_str;
-            // // Tell TWITTER to retweet
-            // Twitter.post('statuses/retweet/:id', {
-            //     id: retweetId
-            // }, function(err, response) {
-            //     if (response) {
-            //         console.log('Retweeted!!!');
-            //     }
-            //     // if there was an error while tweeting
-            //     if (err) {
-            //         console.log('Something went wrong while RETWEETING... Duplication maybe...');
-            //     }
-            // });
+            var retweetId = data.statuses[0].id_str;
+            // Tell TWITTER to retweet
+            Twitter.post('statuses/retweet/:id', {
+                id: retweetId
+            }, function(err, response) {
+                if (response) {
+                    console.log('Retweeted!!!');
+                }
+                // if there was an error while tweeting
+                if (err) {
+                    console.log('Something went wrong while RETWEETING... Duplication maybe...');
+                }
+            });
         }
         // if unable to Search a tweet
         else {
